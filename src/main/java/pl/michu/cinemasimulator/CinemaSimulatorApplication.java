b8971281a6f35aa5420ff1047644ccde32f5d875
Michu <mich1992@wp.pl>
2019-03-02 22:01:40 +0100

    Initial commit

---
+++ src/main/java/pl/michu/cinemasimulator/CinemaSimulatorApplication.java
@@ -0,0 +1,13 @@
+package pl.michu.cinemasimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/michu/cinemasimulator/CinemaSimulatorApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.michu.cinemasimulator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CinemaSimulatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
