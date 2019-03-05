cc4537bb0440362cfc833b68407b7e7abfdc9896
Michu <mich1992@wp.pl>
2019-03-06 00:52:54 +0100

    Changed behaviour if Cinema did not pass the validation

--- src/main/java/pl/michu/cinemasimulator/controllers/CinemaController.java
+++ src/main/java/pl/michu/cinemasimulator/controllers/CinemaController.java
@@ -3,6 +3,7 @@ package pl.michu.cinemasimulator.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import pl.michu.cinemasimulator.services.CinemaService;
 import pl.michu.cinemasimulator.services.MovieService;
 import pl.michu.cinemasimulator.services.ScreeningService;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,26 +32,30 @@ public class CinemaController {
 
 
     @GetMapping("/cinemas")
-    public String getCinemaList(Model model){
+    public String getCinemaList(Model model) {
         List<Cinema> cinemas = cinemaService.findAll();
         model.addAttribute("cinemas", cinemas);
         return "cinemas";
     }
 
     @GetMapping("/newcinema")
-    public String createCinema(Model model){
+    public String createCinema(Model model) {
         model.addAttribute("cinema", new Cinema());
         return "cinemaform";
     }
 
     @PostMapping("/cinemas")
-    public String addCinema(Cinema cinema){
-        cinemaService.addCinema(cinema);
-        return "redirect:/cinemas";
+    public String addCinema(@Valid Cinema cinema, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "cinemaform";
+        } else {
+            cinemaService.addCinema(cinema);
+            return "redirect:/cinemas";
+        }
     }
 
     @GetMapping("/cinema/delete/{id}")
-    public String deleteCinema(@PathVariable(value = "id") Long id){
+    public String deleteCinema(@PathVariable(value = "id") Long id) {
         cinemaService.removeCinemaById(id);
         return "redirect:/cinemas";
     }
