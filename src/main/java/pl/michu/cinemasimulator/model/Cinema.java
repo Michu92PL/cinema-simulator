f7801a747d2ad339f2805b43132fe313fb91df3e
Michu <mich1992@wp.pl>
2019-03-06 00:51:18 +0100

    Added validation, changed getExpectedIncome to return a formatted string instead of Double

--- src/main/java/pl/michu/cinemasimulator/model/Cinema.java
+++ src/main/java/pl/michu/cinemasimulator/model/Cinema.java
@@ -1,6 +1,9 @@
 package pl.michu.cinemasimulator.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +14,14 @@ public class Cinema {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Size(min = 2, max = 40, message = "Nazwa musi mieć między 2 a 40 znaków")
     private String name;
+    @NotNull
+    @Size(min = 5, max = 50, message = "Adres musi mieć między 5 a 50 znaków")
     private String adress;
+    @NotNull
+    @Size(min = 5, max = 12, message = "Nr telefonu musi mieć między 5 a 12 znaków")
     private String telephone;
 
     @OneToMany(mappedBy = "cinema",cascade = CascadeType.ALL)
@@ -52,11 +61,13 @@ public class Cinema {
         return id;
     }
 
-    public Double getExpectedIncome(){
+    public String getExpectedIncome(){
         if(!screenings.isEmpty()) {
-            return screenings.stream().mapToDouble(Screening::getExpectedIncome).reduce(0.0, Double::sum);
+            Double income = screenings.stream().mapToDouble(Screening::getExpectedIncome).reduce(0.0, Double::sum);
+            String roundOff = String.format("%.2f", income).replaceAll(",", ".");
+            return roundOff;
         } else
-            return 0.0;
+            return "0.00";
     }
 
     public void setName(String name) {
